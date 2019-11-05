9f0b9dd698db48b7f4de2c10e4cce090d0e91132
h.odahara <odahara178@gmail.com>
2019-11-05 22:13:53 +0900

    form.blade.phpの追加

---
+++ resources/views/form.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <label class="form-inline">
+        <input class="btn btn-default form-control" type="submit" name="foo" value="Foo">
+        <input class="btn btn-default form-control" type="submit" name="bar" value="Bar">
+    </label>
+</div>
+@endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,6 @@
 
 @section('content')
 <div class="container">
-    <div>aaa</div>
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
