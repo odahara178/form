d6485deec91445b783666b7107b6d719324577b6
h.odahara <odahara178@gmail.com>
2019-11-24 00:17:46 +0900

    aaa

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -5,6 +5,7 @@
     <label class="form-inline">
         <input class="btn btn-default form-control" type="submit" name="foo" value="Foo">
         <input class="btn btn-default form-control" type="submit" name="bar" value="Bar">
+        aaa
     </label>
 </div>
 @endsection
